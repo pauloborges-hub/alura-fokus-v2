534a372cbd8ef6b8565f71752921c93dec6da315
Paulo Borges <victorborges296@hotmail.com>
2024-02-24 15:38:32 -0300

    submetendo o formulário

---
+++ script-crud.js
@@ -0,0 +1,20 @@
+// encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden'); // coloca um toggle no botão de adicionar nova tarefa (mostra/esconde formulário)
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();                        // impede o comportamento padrão do evento (recarregar a página)
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
+})
